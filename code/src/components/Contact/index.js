16f5aa4361b991a3c2aaa6aa2714ec20b821ed06
Yoran <53528221+GlobularShip452@users.noreply.github.com>
2020-10-30 00:11:16 +0100

    Working contact form and updated sidebar.
    Now is the contact form working and the sidebar is updated with an contact link.

--- code/src/components/Contact/index.js
+++ code/src/components/Contact/index.js
@@ -1,9 +1,37 @@
 import React from "react";
+import {
+    Container,
+    Icon,
+    FormWrap,
+    FormContent,
+    Form,
+    FormH1,
+    FormLabel,
+    FormInput,
+    FormButton,
+    FormInputMessage
+  } from "./ContactElements";
 
 const Contact = () => {
   return (
     <>
-      <h1>Hello World, contact in the building!</h1>
+      <Container>
+        <FormWrap>
+          <Icon to="/">YEDevelopment</Icon>
+          <FormContent>
+            <Form action="https://formspree.io/f/xjvprgjr" method="POST">
+              <FormH1>Contact</FormH1>
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput name="email" type="email" required />
+              <FormLabel htmlFor="for">Naam</FormLabel>
+              <FormInput name="naam" type="text" required />
+              <FormLabel htmlFor="for">Bericht</FormLabel>
+              <FormInputMessage name="bericht" htmlFor="for"></FormInputMessage>
+              <FormButton type="submit" value="Send">Verstuur</FormButton>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
     </>
   );
 };
